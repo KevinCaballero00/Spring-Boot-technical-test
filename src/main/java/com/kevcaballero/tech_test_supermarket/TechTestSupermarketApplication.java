9b7ee2600ee51a9202aae3415bf3b2c4ede384ce
KevinCaballero00 <kevinarturocc@ufps.edu.co>
2026-02-04 17:23:30 -0500

    first commit

---
+++ src/main/java/com/kevcaballero/tech_test_supermarket/TechTestSupermarketApplication.java
@@ -0,0 +1,13 @@
+package com.kevcaballero.tech_test_supermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechTestSupermarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechTestSupermarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kevcaballero/tech_test_supermarket/TechTestSupermarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevcaballero.tech_test_supermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechTestSupermarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
